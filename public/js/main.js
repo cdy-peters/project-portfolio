9c60af1fc5be26e21ecc8028d8d01ff213c8d7c0
Cody Peters <cjosephp15@gmail.com>
2022-09-03 02:01:02 +1000

    Selected theme pertains on reload

--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,29 @@
 // Update theme
-var currentTheme = "light";
-if (
-  window.matchMedia &&
-  window.matchMedia("(prefers-color-scheme: dark)").matches
-) {
-  currentTheme = "dark";
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+if (localStorage.getItem("theme") === null) {
+  localStorage.setItem("theme", prefersDarkScheme.matches ? "dark" : "light");
+}
+const currentTheme = localStorage.getItem("theme");
+
+if (currentTheme == "dark") {
   document.getElementById("css").href = "css/style-dark.css";
+} else if (currentTheme == "light") {
+  document.getElementById("css").href = "css/style-light.css";
 }
 
 const themeHandler = () => {
-  if (currentTheme === "light") {
-    currentTheme = "dark";
-    document.getElementById("css").href = "css/style-dark.css";
-  } else {
-    currentTheme = "light";
+  if (currentTheme == "dark") {
+    localStorage.setItem("theme", "light");
     document.getElementById("css").href = "css/style-light.css";
+  } else if (currentTheme == "light") {
+    localStorage.setItem("theme", "dark");
+    document.getElementById("css").href = "css/style-dark.css";
   }
 };
 
 $(document).ready(function () {
+  $("#loading").hide();
+
   $("#fullpage").fullpage({
     // Navigation
     menu: "#menu",
