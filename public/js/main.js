811ec30c28c0647fdd76565c9896087c30a26218
Cody Peters <cjosephp15@gmail.com>
2022-09-02 22:08:52 +1000

    First commit

---
+++ public/js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  $("#fullpage").fullpage({
+    // Navigation
+    menu: "#menu",
+    navigation: true,
+    navigationTooltips: [
+      "Introduction",
+      "Skills",
+      "Recommendations",
+      "Messaging Webapp",
+      "Online Photo Editor",
+      "Choropleth Timelines",
+    ],
+
+    anchors: [
+      "introduction",
+      "skills",
+      "recommendations",
+      "messaging-webapp",
+      "online-photo-editor",
+      "choropleth-timelines",
+    ],
+  });
+});
