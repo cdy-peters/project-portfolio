2e5f75b09cb52afa61e9d4f8004db5f4473489c3
Cody Peters <cjosephp15@gmail.com>
2022-09-22 23:07:55 +1000

    Added routes for hosted web apps

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
+app.get("/choropleth-timelines", (req, res) => {
+  res.redirect("https://choropleth-timelines.vercel.app/");
+});
+
+app.get("/online-photo-editor", (req, res) => {
+  res.redirect("https://online-photo-editor.vercel.app/");
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
